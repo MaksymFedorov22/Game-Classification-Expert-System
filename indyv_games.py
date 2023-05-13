9fa839be48ed4eff0c09551aa80f958a886e3fc5
MaksymFedorov22 <maxf7897@gmail.com>
2023-05-13 18:42:36 +0300

    version_2

--- indyv_games.py
+++ indyv_games.py
@@ -1,327 +1,13 @@
 #
 ##
 
+import questions
+
 #####
 
 print("Вас вітає експертна система класифікації жанрів комп’ютерних ігор !")
 print("Виконав Федоров Максим КНМС-11")
 print("\nВибирайте відповіді ввівши номер варіанту\т")
-
-# Перші питання
-
-first_questions = [
-"""
-Який тип бойовиків вам більше подобається?
-
-1. Аркади й ритм-ігри
-2. Перестріляти всіх
-3. Платформери й бігання
-4. Поєдинки й бойові мистецтва
-5. Рубанина
-6. Стрілянки від першої особи
-7. Стрілянки від третьої особи
-""", 7,
-"""
-Який тип рольових ігор вам більше подобається?
-
-1. Групові ігри
-2. Мандрівні ігри
-3. Покрокові ігри
-4. Пригодницькі рольові ігри
-5. Рольові бойовики
-6. Стратегічні рольові ігри
-7. Масова мережева рольова гра
-""", 7,
-"""
-Який тип стратегій вам більше подобається?
-
-1. Військові стратегії
-2. Містобудівельні ігри
-3. Стратегії в реальному часі
-""", 3,
-"""
-Який тип пригодницьких ігор вам більше подобається?
-
-1. Глибокий сюжет
-2. Головоломки
-3. Казуальні ігри
-4. Метроїдванії
-5. Пригодницькі рольові ігри
-6. Приховані об’єкти
-""", 6,
-"""
-Який тип симуляторів вам більше подобається?
-
-1. Будівництво й автоматизація
-2. Життя й занурення
-3. Космос і літання
-4. Пісочниці й фізика
-5. Фермерство й виготовлення
-""", 5,
-"""
-Який тип спортивних ігор та перегон вам більше подобається?
-
-1. Індивідуальні види спорту
-2. Командний спорт
-3. Перегони
-4. Полювання і риболовля
-""", 4
-]
-
-# Другі питання
-
-second_questions = [
-    [
-"""
-1. Кооперативна гра
-2. Одиночна гра
-""", 2,
-"""
-1. Складна гра
-2. Легка гра
-""", 2,
-"""
-1. Гра головоломка
-2. Гра платформер
-""", 2,
-"""
-1. Двовимірний поєдинок
-2. Симулятор
-""", 2,
-"""
-1. Піксельна графіка
-2. Тривимірна фантастика
-""", 2,
-"""
-1. Реалістична графіка
-2. Мультяшний дизайн
-""", 2,
-"""
-1. Ігри про події сучасного світу
-2. Ігри про події минулого
-""", 2
-    ],
-    [
-"""
-1. Фентезі
-2. Наукова фантастика
-""", 2,
-"""
-1. Наукова фантастика
-2. Фентезі
-""", 2,
-"""
-1. Покрокові бої
-2. Ігри з глибоким сюжетом
-""", 2,
-"""
-1. Глибокий сюжет
-2. Темне фентезі
-3. Магія
-""", 3,
-"""
-1. Стрілянка зі збиранням
-2. Відкритий світ
-""", 2,
-"""
-1. Симулятор
-2. Мультиплеєр
-""", 2,
-"""
-1. Фентезі
-2. Наукова фантастика
-""", 2
-    ],
-    [
-"""
-1. Друга світова
-2. Історія від початку існування людства до майбутнього
-3. Далеке майбутне
-4. Середньовіччя
-""", 4,
-"""
-1. Симулятор колонії
-2. Симулятор сучасного міста
-""", 2,
-"""
-1. Ігри про минуле
-2. Ігри про майбутне
-""", 2
-    ],
-    [
-"""
-1. Містика
-2. Темне фентезі
-3. Минуле
-""", 3,
-"""
-1. Двовимірні Головоломки
-2. Тривимірні головоломки
-""", 2,
-"""
-1. Реалістичні історії
-2. Видумані історії
-""", 2,
-"""
-1. Фентезі
-2. Ретро
-""", 2,
-"""
-1. Глибокий сюжет
-2. Темне фентезі
-3. Магія
-""", 3,
-"""
-1. Стиль графіки малюнок
-2. Стиль графіки мультфільм
-""", 2
-    ],
-    [
-"""
-1. Пісочниця
-2. Автоматизація виробництва
-3. Симулятор колонії
-""", 3,
-"""
-1. Симулятор життя
-2. Симулятор роботи
-""", 2,
-"""
-1. Симулятор космосу
-2. Симулятор авіації
-""", 2,
-"""
-1. Фізичний симулятор
-2. Пісочниця
-""", 2,
-"""
-1. Симулятор техніки
-2. Симулятор фермерства
-""", 2
-    ],
-    [
-"""
-1. Теніс
-2. Гольф
-3. Лижний спорт
-4. Боулінг
-5. Бокс
-""", 5,
-"""
-1. Футбол
-2. Баскетбол
-3. Волейбол
-""", 3,
-"""
-1. Релакс
-2. Симулятор пошкоджень
-3. Дрифт
-4. Симулятор водіння
-""", 4,
-"""
-1. Полювання
-2. Риболовля
-""", 2
-    ]
-]
-
-# Ігри
-
-answers = [
-    [
-        ["CUPHEAD", "GEOMETRY DASH"],
-        ["ENTER THE GUNGEON", "VAMPIRE SURVIVORS"],
-        ["PORTAL", "HOLLOW KNIGHT"],
-        ["BRAWLHALLA", "WWE 2K23"],
-        ["DEAD CELLS", "DEVIL MAY CRY 5"],
-        ["COUNTER-STRIKE: GLOBAL OFFENSIVE", "VALORANT"],
-        ["GRAND THEFT AUTO V", "RED DEAD REDEMPTION 2"]
-    ],
-    [
-        ["DIVINITY: ORIGINAL SIN 2 - DEFINITIVE EDITION", "STAR WARS™ KNIGHTS OF THE OLD REPUBLIC™"],
-        ["STAR WARS JEDI: SURVIVOR™", "ELDEN RING"],
-        ["PATHFINDER: WRATH OF THE RIGHTEOUS - ENHANCED EDITION", "BALDUR'S GATE 3"],
-        ["ВІДЬМАК® 3: ДИКИЙ ГІН", "DARK SOULS™ III", "HOGWARTS LEGACY"],
-        ["WARFRAME", "ELDEN RING"],
-        ["CRUSADER KINGS III", "DOTA 2"],
-        ["THE ELDER SCROLLS® ONLINE", "STAR WARS™: THE OLD REPUBLIC™"]
-    ],
-    [
-        [" HEARTS OF IRON IV", "SID MEIER'S CIVILIZATION® V", "STELLARIS", "CRUSADER KINGS III"],
-        ["RIMWORLD", "CITIES: SKYLINES"],
-        ["AGE OF EMPIRES", "StarCraft II: Wings of Liberty"]
-    ],
-    [
-        ["ВІДЬМАК® 3: ДИКИЙ ГІН", "DARK SOULS™ III", "RED DEAD REDEMPTION 2"],
-        ["RUSTY LAKE", "THE ROOM"],
-        ["The Sims™ 4", "Stardew Valley"],
-        ["ORI AND SERIES", "CASTLEVANIA"],
-        ["ВІДЬМАК® 3: ДИКИЙ ГІН", "DARK SOULS™ III", "HOGWARTS LEGACY"],
-        ["HIDDEN FOLKS", "GRIM LEGENDS"]
-    ],
-    [
-        ["GARRY'S MOD", "FACTORIO", "RIMWORLD"],
-        ["THE SIMS™ 4", "одна з списку на цій сторінці: \nhttps://store.steampowered.com/category/sim_life/"],
-        ["KERBAL SPACE PROGRAM", "MICROSOFT FLIGHT SIMULATOR"],
-        ["GARRY'S MOD", "MINECRAFT"],
-        ["FARMING SIMULATOR", "STARDEW VALLEY"]  
-    ],
-    [
-        ["VIRTUA TENNIS", "PGA TOUR 2K21", "STEEP", "PBA PRO BOWLING 2021", "EA SPORTS UFC"],
-        ["FIFA", "NBA 2K", "Spike Volleyball"],
-        ["FORZA HORIZON", "BEAMNG.DRIVE", "CARX DRIFT RACING ONLINE", "ASSETTO CORSA"],
-        ["Hunting Simulator 2", "FISHING PLANET"]
-    ]
-]
-
-# Посилання
-
-links = [
-    [
-        "https://store.steampowered.com/category/arcade_rhythm/",
-        "https://store.steampowered.com/category/shmup/",
-        "https://store.steampowered.com/category/action_run_jump/",
-        "https://store.steampowered.com/category/fighting_martial_arts/",
-        "https://store.steampowered.com/category/hack_and_slash/",
-        "https://store.steampowered.com/category/action_fps/",
-        "https://store.steampowered.com/category/action_tps/"
-    ],
-    [
-        "https://store.steampowered.com/category/rpg_party_based/",
-        "https://store.steampowered.com/category/rogue_like_rogue_lite/",
-        "https://store.steampowered.com/category/rpg_turn_based/",
-        "https://store.steampowered.com/category/adventure_rpg/",
-        "https://store.steampowered.com/category/rpg_action/",
-        "https://store.steampowered.com/category/rpg_strategy_tactics/",
-        "https://store.steampowered.com/tags/en/MMORPG/"
-    ],
-    [
-        "https://store.steampowered.com/category/strategy_military/",
-        "https://store.steampowered.com/category/strategy_cities_settlements/",
-        "https://store.steampowered.com/category/strategy_real_time/"
-    ],
-    [
-        "https://store.steampowered.com/category/story_rich/",
-        "https://store.steampowered.com/category/puzzle_matching/",
-        "https://store.steampowered.com/category/casual/",
-        "https://store.steampowered.com/category/metroidvania/",
-        "https://store.steampowered.com/category/adventure_rpg/",
-        "https://store.steampowered.com/category/hidden_object/"
-    ],
-    [
-        "https://store.steampowered.com/category/sim_building_automation/",
-        "https://store.steampowered.com/category/sim_life/",
-        "https://store.steampowered.com/category/sim_space_flight/",
-        "https://store.steampowered.com/category/sim_physics_sandbox/",
-        "https://store.steampowered.com/category/sim_farming_crafting/"
-    ],
-    [
-        "https://store.steampowered.com/category/sports_individual/",
-        "https://store.steampowered.com/category/sports_team/",
-        "https://store.steampowered.com/category/racing/",
-        "https://store.steampowered.com/category/sports_fishing_hunting/"
-    ]
-]
 
 #####
 
@@ -357,13 +43,13 @@ res.append(inp)
 
 # Перше питання
 
-print(first_questions[res[0] * 2 - 2])
+print(questions.first_questions[res[0] * 2 - 2])
 
 chk = True
 while chk:
     try:
         inp = int(input())
-        if not inp in range(1, first_questions[res[0] * 2 - 1] + 1):
+        if not inp in range(1, questions.first_questions[res[0] * 2 - 1] + 1):
             print("Введіть номер варіанту")
         else:
             chk = False
@@ -376,13 +62,13 @@ res.append(inp)
 
 print("\nВиберіть варіант, який вам більше подобається")
 
-print(second_questions[res[0] - 1][res[1] * 2 - 2])
+print(questions.second_questions[res[0] - 1][res[1] * 2 - 2])
 
 chk = True
 while chk:
     try:
         inp = int(input())
-        if not inp in range(1, second_questions[res[0] - 1][res[1] * 2 - 1] + 1):
+        if not inp in range(1, questions.second_questions[res[0] - 1][res[1] * 2 - 1] + 1):
             print("Введіть номер варіанту")
         else:
             chk = False
@@ -392,9 +78,9 @@ while chk:
 res.append(inp)
 
 print("\nНа основі ваших відповідей найкращою грою для вас буде " 
-      + answers[res[0] - 1][res[1] - 1][res[2] - 1] + 
+      + questions.answers[res[0] - 1][res[1] - 1][res[2] - 1] + 
       "\n\nЯкщо вам не сподобалася запропонована гра, ось посилання на сайт з подібними іграми: "
-      + links[res[0] - 1][res[1] - 1])
+      + questions.links[res[0] - 1][res[1] - 1])
 
 ##
 #
